d663419e4ed0d49294e50536ae99db1e31234be0
ifotn <rich@infrontofthenet.com>
2016-03-15 22:21:59 -0400

    first commit

---
+++ config/db.js
@@ -0,0 +1,12 @@
+// create public connection string
+module.exports = {
+
+    // local
+    // 'url': 'mongodb://localhost/test'
+
+    // live on mLab
+    'url': 'mongodb://gcrfreeman:2106pass@ds056288.mlab.com:56288/comp2106',
+    'githubClientId': '164e067e3f1ced9614a1',
+    'githubClientSecret': '5c81df4dc16ed215a8fab025bb9be3d1b149f689',
+    'githubCallbackUrl': 'http://localhost:3000/auth/github/callback'
+};
